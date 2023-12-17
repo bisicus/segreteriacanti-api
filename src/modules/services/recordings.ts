60955c11bb3fda2a8b4f05a01977f81cf6d1e13f
cattoblefa <leonardo.bisica@gmail.com>
2023-12-17 19:12:26 +0100

    logging

--- src/modules/services/recordings.ts
+++ src/modules/services/recordings.ts
@@ -7,6 +7,7 @@ import path from 'path';
 import { config } from '../../config';
 import { BaseError } from '../../errors/BaseError';
 import { db } from '../db';
+import logger from '../logger';
 import type { RegistrazioneConTitoloCanto } from '../models/recordings';
 import { forgeFilename } from '../models/recordings';
 
@@ -19,6 +20,8 @@ import { forgeFilename } from '../models/recordings';
  * @todo implementare un "Cestino delle registrazioni?"
  */
 export const linkUploadedFile = async (recordingId: number, file: Express.Multer.File): Promise<Registrazione> => {
+  logger.debug({ filename: file.filename, mime: file.mimetype, recordId: recordingId }, 'gestione del file per la registrazione');
+
   const DBRegistrazioneConTitolo: RegistrazioneConTitoloCanto | null = await db.registrazione.findUnique({
     where: {
       id: recordingId,
@@ -40,15 +43,24 @@ export const linkUploadedFile = async (recordingId: number, file: Express.Multer
   const _fileName = forgeFilename(DBRegistrazioneConTitolo);
   const _fileExtension = extension(file.mimetype);
   const filePath = `${_fileName}.${_fileExtension}`;
+  logger.debug({ path: filePath, recordId: recordingId }, 'nuovo path per il file');
 
   // Muovi il file nella cartella delle registrazioni
-  await fs.move(file.path, path.join(config.storage.recordings, filePath), {
-    overwrite: true,
-  });
+  const _destinationPath = path.join(config.storage.recordings, filePath);
+  logger.debug({ oldPath: file.path, newPath: _destinationPath, recordId: recordingId }, 'sostituzione file');
+
+  try {
+    await fs.move(file.path, _destinationPath, { overwrite: true });
+  } catch (err) {
+    logger.error({ err: err, oldPath: file.path, newPath: _destinationPath, recordId: recordingId }, 'impossibile sostituire il file');
+    throw err;
+  }
 
   // aggiorna il modell
   let registrazioneUpdated: Registrazione;
   if (filePath !== DBRegistrazioneConTitolo.refAudio) {
+    logger.info({ oldRef: DBRegistrazioneConTitolo.refAudio, newRef: filePath, recordId: recordingId }, 'sostituzione di audio ref');
+
     // trattieni vecchio file
     const oldRef = DBRegistrazioneConTitolo.refAudio;
 
@@ -61,13 +73,24 @@ export const linkUploadedFile = async (recordingId: number, file: Express.Multer
         refAudio: filePath,
       },
     });
+    logger.debug({ newRef: filePath, recordId: recordingId }, 'audio ref sostituita correttamente');
 
     // cancella vecchio file
     if (oldRef) {
-      await fs.remove(path.join(config.storage.recordings, oldRef));
+      const _removePath = path.join(config.storage.recordings, oldRef);
+      logger.debug({ path: _removePath, recordId: recordingId }, 'rimozione audio ref');
+
+      try {
+        await fs.remove(path.join(config.storage.recordings, oldRef));
+      } catch (err) {
+        logger.fatal({ err: err, path: _removePath, recordId: recordingId }, 'impossibile rimuovere audio ref');
+        // non è necessario interrompere il flusso; accumuliamo solo sporcizia.
+      }
     }
   } else {
     // riutilizza il modello preso dal DB
+    logger.debug({ ref: DBRegistrazioneConTitolo.refAudio, recordId: recordingId }, 'audio ref non modificata dalla richiesta');
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { canto, ..._r } = DBRegistrazioneConTitolo;
     registrazioneUpdated = _r;
