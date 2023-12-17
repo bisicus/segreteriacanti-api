e4e667e3fb04bb3ce1dbe84a2db21903838a18dc
cattoblefa <leonardo.bisica@gmail.com>
2023-12-17 17:08:50 +0100

    add storage to config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import path from 'path';
 import type { Level } from 'pino';
 
 // Load .env
@@ -11,6 +12,13 @@ const missingRequiredEnvVars = requiredEnvVars.filter((_envVar) => !(_envVar in 
 if (missingRequiredEnvVars.length !== 0) {
   throw new Error(`missing required env variables [${missingRequiredEnvVars.join(', ')}]`);
 }
+
+// reusable variables //
+const defaults__folders_storageMain = path.join(process.cwd(), 'storage');
+
+//////////////////////
+/////   CONFIG   /////
+//////////////////////
 
 export const config = {
   NODE_ENV: process.env['NODE_ENV'] || 'production',
@@ -28,5 +36,11 @@ export const config = {
     prettyPrint: ['yes', 'y', '1', 'true'].includes(String(process.env['LOGGING__PRETTYPRINT']).toLowerCase()),
     printLevelAsLabel: ['yes', 'y', '1', 'true'].includes(String(process.env['LOGGING__LEVEL_AS_LABEL']).toLowerCase()),
     printTime: process.env['LOGGING__PRINT_TIME'] || 'epoch',
+  },
+
+  storage: {
+    main: defaults__folders_storageMain,
+    recordings: path.join(defaults__folders_storageMain, 'recordings'),
+    tmp: path.join(defaults__folders_storageMain, 'tmp'),
   },
 };
