aecc638048e35f771fb5c0df315bb682b86ec84d
cattoblefa <leonardo.bisica@gmail.com>
2023-12-23 09:47:48 +0100

    import data handles csv file

--- src/controllers/v1/import.ts
+++ src/controllers/v1/import.ts
@@ -2,6 +2,7 @@ import type { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import { BaseError } from '../../errors/BaseError';
+import { requestCsvToImportJson } from '../../modules/request-helper/import';
 import { importRecords } from '../../modules/services/import';
 
 /**
@@ -9,18 +10,25 @@ import { importRecords } from '../../modules/services/import';
  * Input is an object array; each array element has the same structure.
  * @since 1.0.0
  * @todo validation of every element of the array (evaluate `express-validator` or `zod`)
- * @todo support CSV import
  * @todo support XLSX import
  * @todo localization
  * @todo replace BaseError with more specific error
  */
 export const importData: RequestHandler = async (req, res, next) => {
   try {
-    if (Array.isArray(req.body) === false) {
-      throw new BaseError('validation', 'invalid input', StatusCodes.BAD_REQUEST);
+    let importObject;
+    if (Object.keys(req.body).length) {
+      if (Array.isArray(req.body) === false) {
+        throw new BaseError('validation', 'invalid input', StatusCodes.BAD_REQUEST);
+      }
+      importObject = req.body;
+    } else if (req.file) {
+      importObject = await requestCsvToImportJson(req.file);
+    } else {
+      throw new BaseError('validation', 'missing input', StatusCodes.BAD_REQUEST);
     }
 
-    const newRecordings = await importRecords(req.body);
+    const newRecordings = await importRecords(importObject);
 
     res.status(StatusCodes.CREATED).json(newRecordings);
   } catch (error) {
