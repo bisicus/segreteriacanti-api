9dfb6a082796d2a4f45020be41abe183ca6e04fd
cattoblefa <leonardo.bisica@gmail.com>
2023-12-24 08:27:01 +0100

    validate function using zod

---
+++ src/modules/validate/index.ts
@@ -0,0 +1,17 @@
+import type { AnyZodObject, z, ZodArray } from 'zod';
+
+import ValidationError from '../../errors/ValidationError';
+
+/**
+ * @throws {ValidationError}
+ * @since 1.0.0
+ * @todo wrap ZodError in custom error object
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function validate<T extends AnyZodObject | ZodArray<any, any>>(data: any, schema: T): z.infer<T> {
+  const validateResult = schema.safeParse(data);
+  if (validateResult.success === false) {
+    throw new ValidationError(validateResult.error);
+  }
+  return data;
+}
