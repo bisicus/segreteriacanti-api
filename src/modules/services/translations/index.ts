634bf87a7fb06582715b0f80190f70638d8719da
cattoblefa <leonardo.bisica@gmail.com>
2024-02-06 23:50:03 +0100

    language extraction validates agains ISO 639-1

--- src/modules/services/translations/index.ts
+++ src/modules/services/translations/index.ts
@@ -2,23 +2,28 @@ import { StatusCodes } from 'http-status-codes';
 
 import { config } from '../../../config';
 import { BaseError } from '../../../errors/BaseError';
+import { getIso639_1 } from '../../languages/iso639-1';
 
 export * from './link-files';
 
 /**
  * creates unique name for file ref using song title, language and extension of input file
  * @since 1.0.0
- * @todo hard-constrained type for `language` (along with validation)
  */
 export const extractLanguageFromFilename = (file: Express.Multer.File): string => {
   // remove extension
   const filename = file.originalname.split('.').at(0) as string;
 
-  let language = filename.split(config.application.song.translations.filenameSeparator).at(-1);
+  const language = filename.split(config.application.song.translations.filenameSeparator).at(-1);
   if (!language) {
-    throw new BaseError('valiidation', 'filename does not contain a valid language', StatusCodes.BAD_REQUEST);
+    throw new BaseError('valiidation', 'filename does not contain a language', StatusCodes.BAD_REQUEST);
   }
 
-  language = language.toLowerCase();
-  return language;
+  // validate language
+  const parsedLanguage = getIso639_1(language);
+  if (!parsedLanguage) {
+    throw new BaseError('valiidation', `invalid language code '${language}'`, StatusCodes.BAD_REQUEST);
+  }
+
+  return parsedLanguage.toLowerCase();
 };
