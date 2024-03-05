2589aac1055aac3205973d2efb190ec292de3657
cattoblefa <leonardo.bisica@gmail.com>
2024-03-05 19:29:40 +0100

    ✨ add generic util type to extract array from "array or non-array"

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,14 @@
 export type ExtractPropertiesWithPrefix<T, Prefix extends string> = {
   [K in keyof T]: K extends `${Prefix}${string}` ? K : never;
 }[keyof T];
+
+/**
+ * Extracts the Array from an "array or non-array" type
+ * @example
+ * type OnlyStringArray = ExtractArrayType<string|string[]> // string[]
+ *
+ * type MyType = { name: string; age: number };
+ * type OnlyStringArray = ExtractArrayType<MyType|MyType[]> // MyType[]
+ * @since 1.0.0
+ */
+export type ExtractArrayType<T> = T extends (infer U)[] ? U[] : never;
