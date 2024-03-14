d6cf21e1ca9931d4eecc811d816a45b0bd40e0b4
cattoblefa <leonardo.bisica@gmail.com>
2024-03-14 22:39:42 +0100

    🚧 add list for recordings

--- src/modules/services/recording/index.ts
+++ src/modules/services/recording/index.ts
@@ -13,6 +13,17 @@ import { forgeFilename } from '../../models/recording';
 import { recordingToPublic } from '../../to-public/recording';
 
 /**
+ * @since 1.0.0
+ * @todo add filters
+ */
+export const listRecordingsToPublic = async (moduleAssets: ModuleAssets) => {
+  const DbRecordingList = await db.recording.findMany();
+
+  // public interface
+  return DbRecordingList.map((DbRecording) => recordingToPublic(moduleAssets, DbRecording));
+};
+
+/**
  * Ritorna l'oggetto che descrive una registrazione. Allega eventualmente le entità relazionate.
  * @since 1.0.0
  */
